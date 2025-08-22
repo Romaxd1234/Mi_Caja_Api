040b13d0c04079eb584a2fd498608000c6e5c89f
Romaxd1234 <gamersjuegosytutoriales41@gmail.com>
2025-08-22 14:23:44 -0600

    Arreglada la URL de conexión a Render PostgreSQL

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from databases import Database
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Float, JSON
 
-DATABASE_URL = "postgres://invex_db_user:uR4BGxN3VLcanhCdVSO6laFUgtzzCiwG@dpg-d2kacbv5r7bs73ekh5bg-a:5432/invex_db"
+DATABASE_URL = "postgresql://invex_db_user:uR4BGxN3VLcanhCdVSO6laFUgtzzCiwG@dpg-d2kacbv5r7bs73ekh5bg-a.oregon-postgres.render.com:5432/invex_db"
 
 # Conexión asíncrona para FastAPI
 database = Database(DATABASE_URL)
